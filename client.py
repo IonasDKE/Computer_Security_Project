ca2c7517df599cc65ca524ce8061f2c34fa3d5c8
ionas fabris <ionasfabris3@gmai.com>
2021-09-26 10:33:57 +0200

    fixed the issues

--- client.py
+++ client.py
@@ -1,86 +1,105 @@
-import socket, json
+import socket, json, time
 
 class Client():
 
-  def __init__(self, client_file, sock=None):
+	def __init__(self, client_file, sock=None):
 
-    if sock == None:
-      self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    else:
-      self.s = sock
+		if sock == None:
+			self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		else:
+			self.s = sock
 
-    client_info = self.setClientInfo(client_file)
+		client_info = self.setClientInfo(client_file)
 
-    # Connect to the server 
-    print("-connecting to ip: " + self.ip + ', socket: ' + str(self.port))
-    print()
-    self.s.connect((self.ip, self.port))
+		# Connect to the server 
+		print("-connecting to ip: " + self.ip + ', socket: ' + str(self.port))
+		print()
+		self.s.connect((self.ip, self.port))
 
-    # Register client to the server
-    self.register()
+		# Register client to the server
+		self.register()
 
-    # Perform the client actions
-    if self.actions != None:
-      for action in self.actions:
-        #split the action and the amount
-        tmp = action.split()
-        action_type = tmp[0]
-        action_amount = tmp[1]
+		# Perform the client actions
+		action_counter = 0
+		if self.actions != None:
+			for action in self.actions:
+				#split the action and the amount
+				tmp = action.split()
+				action_type = tmp[0]
+				action_amount = tmp[1]
 
-        if action_type == 'INCREASE':
-          self.Increase(action_amount)
+				action_counter += 1
+				print(str(action_counter) + ' ' + action_type)
 
-        elif action_type == 'DECREASE':
-          self.Decrease(action_amount)
+				if action_type == 'INCREASE':
+					print('increase')
+					self.Increase(action_amount)
 
-        else:
-          raise NameError("action type does not exist: " + action_type)
+				elif action_type == 'DECREASE':
+					print('decrease')
+					self.Decrease(action_amount)
 
-    else:
-      print("no action found")
+				else:
+					raise NameError("action type does not exist: " + action_type)
 
-    # disconnect the client from the server
-    self.s.close()
+		else:
+			print("no action found")
+
+		# disconnect the client from the server
+		self.s.close()
+		
+
+	# Register the client to the server
+	def register(self):
+		message = 'register,' + self.id + ',' + self.password
+		self.s.send(message.encode('utf-8'))
+
+		answer = self.s.recv(1024).decode('utf-8')
+		print(answer)
+		
+
+	# Increase the client counter
+	def Increase(self, amount):
+		self.delay(int(self.action_delay))
+		message = 'INCREASE,' + self.id + ',' + amount
+		self.s.send(message.encode('utf-8'))
+		answer = self.s.recv(1024).decode('utf-8')
+		print(answer)
+		return 
+		
+
+	# decreases the client counter
+	def Decrease(self, amount):
+		self.delay(int(self.action_delay))
+		message = 'DECREASE,' + self.id + ','  + amount 
+		self.s.send(message.encode('utf-8'))
+		answer = self.s.recv(1024).decode('utf-8')
+		print(answer)
+		return 
 
 
-  # Register the client to the server
-  def register(self):
-    message = 'register,' + self.id + ',' + self.password
-    self.s.send(message.encode('utf-8'))
-
-    answer = self.s.recv(1024).decode('utf-8')
-    print(answer)
+	# delay the messages
+	def delay(self, amount):
+		print("Delay: " + str(amount))
+		time.sleep(amount)
+		return
 
 
-  # Increase the client counter
-  def Increase(self, amount):
-    self.action_delay
-    #TODO
-    return 
+	# Extract the informations from the json file
+	def setClientInfo(self, file):
+		with open(file) as json_file:
+			data = json.load(json_file)
 
+			self.port = int(data['server']['port'])
+			self.ip = data['server']['ip']
 
-  # decreases the client counter
-  def Decrease(self, amount):
-    self.action_delay
-    #TODO
-    return 
-
-
-  # Extract the informations from the json file
-  def setClientInfo(self, file):
-    with open(file) as json_file:
-      data = json.load(json_file)
-
-      self.port = int(data['server']['port'])
-      self.ip = data['server']['ip']
-
-      self.id = data['id']
-      self.password = data['password']
-
-      self.action_delay = data['action']['delay']
-      self.actions = data['action']['steps']
-
+			self.id = data['id']
+			self.password = data['password']
+			
+			self.action_delay = data['action']['delay']
+			self.actions = data['action']['steps']
+			
 
 if __name__ == '__main__':
-  file = "client1.json"
-  client = Client(file)
+	file = "client1.json"
+	client = Client(file)
--- server.py
+++ server.py
@@ -1,58 +1,66 @@
 import socket, json
+from threading import Thread
 
 class Server():
-  def __init__(self, port):
+	def __init__(self, port):
 
-    self.registered = {} # list of registered client
-    self.counters = {}
+		self.registered = {} # list of registered client
+		self.counters = {}
 
-    self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    self.s.bind(('127.0.0.1', port))
-    self.s.listen(1)
-    self.port = port
+		self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		self.s.bind(('127.0.0.1', port))
+		self.s.listen(1)
+		self.port = port
 
-    self.run()
+		self.run()
+		
 
+	def run(self):
+		print('waiting for incoming')
+		# client_socket, adress = self.s.accept() 
+		while True:
+			(conn, (ip, port)) = self.s.accept()
+			Thread(target = self.handle_incoming, args=(conn, ip, port)).start()
 
-  def run(self):
-    print('waiting for incoming')
-    # client_socket, adress = self.s.accept() 
-    while True:
-      (conn, (ip, port)) = self.s.accept()
-      # data = client_socket.recv(1024).decode('utf-8').split(',')
-      data = conn.recv(1024).decode('utf-8').split(',')
-      print(data)
+	def handle_incoming(self, conn, ip, port):
+		while True:
+			# data = client_socket.recv(1024).decode('utf-8').split(',')
+			data = conn.recv(1024).decode('utf-8').split(',')
 
-      if not data:
-        break
+			if not data:
+				return
 
-      elif data[0] == 'register':
-        if data[0] in self.registered: # Check if the id already exist in the dictionnary 
-          response = "ERROR: id already exist, please choose another id"
-          print("send answer to client")
-          conn.send(response.encode('utf-8'))
+			elif data[0] == 'register':
+				print('start register process')
 
-        else: # Create the new client and set his counter to 0
-          self.registered[data[1]] = data[2]
-          self.counters[data[1]] = 0
+				if data[0] in self.registered: # Check if the id already exist in the dictionnary 
+					response = "ERROR: id already exist, please choose another id"
+					print("send answer to client")
+					conn.send(response.encode('utf-8'))
 
-          # Send confirmation message to client
-          response = 'registration completed'
-          print("sent answer to client")
-          conn.send(response.encode('utf-8'))
+				else: # Create the new client and set his counter to 0
+					self.registered[data[1]] = data[2]
+					self.counters[data[1]] = 0
 
-      elif data[0] == 'INCREASE':
-        #TODO
+					# Send confirmation message to client
+					response = 'registration completed'
+					print("registration done")
+					conn.send(response.encode('utf-8'))
 
-        response = "counter increased succesfully"
-        conn.send(response.encode('utf-8'))
+			elif data[0] == 'INCREASE':
+				print('increase')
 
-      elif data[0] == 'DECREASE':
-        #TODO
+				self.counters[data[1]] = self.counters[data[1]] + int(data[2])
+				response = "counter increased by: " + data[2]
+				conn.send(response.encode('utf-8'))
 
-        response = "counter decreased succesfully"
-        conn.send(response.encode('utf-8'))
+			elif data[0] == 'DECREASE':
+				print('decrease')
+
+				self.counters[data[1]] = self.counters[data[1]] - int(data[2])
+				response = "counter decreased by: " + data[2]
+				conn.send(response.encode('utf-8'))
 
 
 if __name__ == '__main__':
-  server = Server(10050)
+	server = Server(10050)
